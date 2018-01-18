2325e45f580b06dce4dd9c76647e517c91738d21
doubleu-gee <doubleu.gee10@gmail.com>
2018-01-18 13:40:29 +0200

    built <ProductItem />

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from './product-item.scss';
+
+const ProductItem = ({ name, price, image }) => (
+  <li className={styles.productItem}>
+    <Link className={styles.prodLink} to={`/products?name=${name}`}>
+      <div className={styles.prodInfo}>
+        <h3 className={styles.prodName}>{name}</h3>
+        <span className={`${styles.prodPrice}$`}>{price}</span>
+      </div>
+      <div className={styles.prodImage}>
+        <img className={styles.image} src={image} alt="product pic" />
+      </div>
+    </Link>
+  </li>
+);
+
+ProductItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
+
+export default ProductItem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import SearchBar from '../components/SearchBar/SearchBar';
+import ProductItem from '../components/ProductItem/ProductItem';
 import styles from './home.scss';
 
 const Home = () => (
   <section className={styles.wrapper}>
-    <h1 className={styles.heading}>
+    {/* <h1 className={styles.heading}>
       We help you to shop the right product at the right price!
-    </h1>
-    <SearchBar value="" onChange={() => console.log('yay')} />
+    </h1> */}
+    <ProductItem name="Delta Plus Polycarbonate Visor 2 Piece Set - Clear" price='30$' image="http://cf5.s3.souqcdn.com/item/2015/10/18/92/99/51/9/item_L_9299519_10086890.jpg"/>
   </section>
 );
 
